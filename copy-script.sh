1f57bce0e8cffdb7b6ac8405c772b55f4be2bea1
Cloud User <cloud-user@bastion.example.com>
2022-06-17 04:54:57 -0400

    Update

--- copy-script.sh
+++ copy-script.sh
@@ -1,8 +1,9 @@
 # This repository is to create frontend and backend for AI/ML Object Detection workload in OpenShift cluster
 #!/usr/bin/bash
 export USERNAME=user2
-mkdir $HOME/rhods-project-yaml
-git clone git@github.com:ritzshah/rhods-project-yaml.git
+cd $HOME
+#mkdir $HOME/rhods-project-yaml
+git clone https://github.com/ritzshah/rhods-project-yaml.git
 sed -i "s/user1/$USERNAME/g" $HOME/rhods-project-yaml/object-detection-rest-deployment.yaml
 sed -i "s/user1/$USERNAME/g" $HOME/rhods-project-yaml/object-detection-app-git-deployment.yaml
 for i in `cat list`; do oc delete  -f $i; done
