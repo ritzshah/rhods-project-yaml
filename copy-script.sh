da61956f3681496fddbae222292ae7a1b72a7370
Ritesh Shah <9796427+ritzshah@users.noreply.github.com>
2022-06-17 14:19:43 +0530

    Update copy-script.sh

--- copy-script.sh
+++ copy-script.sh
@@ -1,10 +1,10 @@
 # This repository is to create frontend and backend for AI/ML Object Detection workload in OpenShift cluster
 #!/usr/bin/bash
 export USERNAME=user2
-mkdir $HOME/rhods-project
+mkdir $HOME/rhods-project-yaml
 git clone git@github.com:ritzshah/rhods-project-yaml.git
-sed -i "s/user1/$USERNAME/g" $HOME/rhods-project/object-detection-rest-deployment.yaml
-sed -i "s/user1/$USERNAME/g" $HOME/rhods-project/object-detection-app-git-deployment.yaml
+sed -i "s/user1/$USERNAME/g" $HOME/rhods-project-yaml/object-detection-rest-deployment.yaml
+sed -i "s/user1/$USERNAME/g" $HOME/rhods-project-yaml/object-detection-app-git-deployment.yaml
 for i in `cat list`; do oc delete  -f $i; done
 oc start-build object-detection-rest --wait
 oc start-build object-detection-app-git --wait
